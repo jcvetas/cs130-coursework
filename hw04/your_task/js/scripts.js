b2a6f5901dc3e8f173cf216ff31a7d40950975a0
jcvetas <jake.cvetas@Jakes-MacBook-Pro.local>
2021-05-24 16:53:59 -0500

    finishing hw4

--- hw04/your_task/js/scripts.js
+++ hw04/your_task/js/scripts.js
@@ -34,7 +34,7 @@ const getTracks = (term) => {
                     }
                     else {
                         document.getElementById("tracks").innerHTML += `
-                        <section class="track-item preview" data-preview-track=${track.preview_url}>
+                        <section class="track-item preview" data-preview-track=${track.preview_url} onclick = "connectTrack(event)">
                             <img src=${track.album.image_url}>
                             <i class="fas play-track fa-play" aria-hidden="true"></i>
                             <div class="label">
@@ -52,10 +52,32 @@ const getTracks = (term) => {
 };
 
 const getAlbums = (term) => {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    fetch("https://www.apitutor.org/spotify/simple/v1/search?type=album&q=".concat(term))
+        .then((response) => {
+            return response.json();
+        })
+        .then((albums) => {
+            if (albums.length == 0) {
+                document.getElementById("albums").innerHTML = "No albums returned"
+            }
+            else {
+                document.getElementById("albums").innerHTML = "";
+                for (const alb of albums) {
+                    document.getElementById("albums").innerHTML += `
+                    <section class="album-card" id=${alb.id}>
+                        <div>
+                            <img src=${alb.image_url}>
+                            <h3>${alb.name}</h3>
+                            <div class="footer">
+                                <a href=${alb.spotify_url} target="_blank">
+                                    view on spotify
+                                </a>
+                            </div>
+                        </div>
+                    </section>`;
+                }
+            }
+        })
 };
 
 const getArtist = (term) => {
@@ -94,3 +116,16 @@ document.querySelector('#search').onkeyup = (ev) => {
         search();
     }
 };
+
+const connectTrack = (ev) => {
+    const elem = ev.currentTarget;
+    document.querySelector("footer .track-item").innerHTML = elem.innerHTML;
+    const previewURL = elem.dataset.previewTrack;
+    if (previewURL) {
+        audioPlayer.setAudioFile(previewURL);
+        audioPlayer.play();
+    }
+    else {
+        console.log("There is no preview available for this track")
+    }
+}
